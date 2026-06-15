51830b8cd921c92ea8877415a5001a3f283699ec
Abdulrahim Muhammad <abdelrheem2204@gmail.com>
2026-06-15 11:46:02 +0300

    first commit

---
+++ sentenceMaker.js
@@ -0,0 +1,21 @@
+let adjective = "giant";
+let noun = "dragon";
+let verb = "angry";
+let place = "castle";
+let adjective2 = "smoky";
+let noun2 = "pizza";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". " + "The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+adjective = "tiny";
+noun = "rabbit";
+verb = "excited";
+place = "forest";
+adjective2 = "pink";
+noun2 = "carrots";
+
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+console.log(`Second story: ${secondStory}`);
---
+++ triviaBot.js
@@ -0,0 +1,23 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "AVALEY";
+let botLocation = "the digital world";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+// Fact one
+let codingFact = "did you know that JavaScript was created in just 10 days?";
+console.log(codingFact);
+
+// Fact two
+codingFact = "JavaScript is the most popular programming language in the world!";
+console.log(codingFact);
+
+// Fact three
+codingFact = "JavaScript can be used for both front-end and back-end development.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+

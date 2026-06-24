b0161fe753b71d3b46275e73ce72d838c56e7197
Abdulrahim Muhammad <abdelrheem2204@gmail.com>
2026-06-24 22:49:12 +0300

    2026/06/24

--- DOM/main.js
+++ DOM/main.js
@@ -10,6 +10,24 @@
 
 // toggleBtn.addEventListener("click", () => menu.classList.toggle("show"));
 
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("DOM is loaded.");
+// document.addEventListener("DOMContentLoaded", function () {
+//   console.log("DOM is loaded.");
+// });
+
+// const canvas = document.getElementById("my-canvas");
+// const ctx = canvas.getContext('2d');
+// console.log(ctx);
+// ctx.direction = 'rtl';
+// ctx.fillStyle = 'crimson';
+// ctx.fillText("Abdulrahim", 1, 50);
+const dialog = document.getElementById("modal");
+const openButton = document.getElementById("open-modal-btn");
+const closeButton = document.getElementById("close-modal-btn");
+
+openButton.addEventListener("click", () => {
+  dialog.show();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
 });
